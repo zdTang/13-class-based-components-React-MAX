6215ff9d816cac15870f28bd59e0720b80367e10
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-18 16:44:28 -0500

    003 Adding a First Class-based Component
    for Class based component, " this" is necessary

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,12 @@
-import classes from './User.module.css';
+import classes from "./User.module.css";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+import React, { Component } from "react";
+
+export class User extends Component {
+  // must have the "This"
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
 
 export default User;
